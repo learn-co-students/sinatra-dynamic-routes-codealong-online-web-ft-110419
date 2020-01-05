967b4f40092e0609cd42cbbd486342e0b74e958a
Ethan <ethan_gustafson@outlook.com>
2020-01-05 14:44:31 -0500

    Done.

--- app.rb
+++ app.rb
@@ -7,12 +7,23 @@ class App < Sinatra::Base
     "Hello World!"
   end
 
-  # This is a sample dynamic route.
   get "/hello/:name" do
-    @user_name = params[:name]
+    @user_name = params[:name] # get "/hello/:name" is an example of a dynamic route.
     "Hello #{@user_name}!"
   end
 
   # Code your final two routes here:
+
+  get "/goodbye/:name" do
+    @user_name = params[:name]
+    "Goodbye, #{@user_name}."
+  end
+
+  get "/multiply/:num1/:num2" do
+    @num1 = params[:num1].to_i # params return value will always be a string.
+    @num2 = params[:num2].to_i # you have to convert it into an integer with to_i.
+    result = "#{@num1 * @num2}" # then interpolate the two, reconverting it back into a string.
+    result
+  end
 
 end
